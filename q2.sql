5df14663757b53376024cf1f6237cbf5b3be7f30
charlieluck <chaoshi@umich.edu>
2017-01-30 18:38:20 -0500

    debug in q2.sql

--- q2.sql
+++ q2.sql
@@ -3,6 +3,6 @@ FROM STUDENT A, STUDENT B, MEMBER MA, MEMBER MB,
 	 COURSE C1, COURSE C2, COURSE C3,
 	 ENROLLED E1, ENROLLED E2, ENROLLED E3 
 WHERE A.sid <> B.sid AND A.sid = MA.sid AND B.sid = MB.sid AND MA.pid = MB.pid AND
-	  A.sid = E1.sid AND E1.cid = C1.cid AND (C1.title = 'EECS482' OR C1.title = 'EECS483') AND
-	  A.sid = E2.sid AND E2.cid = C2.cid AND (C2.title = 'EECS484' OR C2.title = 'EECS485') AND
-	  A.sid = E3.sid AND E3.cid = C3.cid AND C3.title = 'EECS280';
+	  B.sid = E1.sid AND E1.cid = C1.cid AND (C1.title = 'EECS482' OR C1.title = 'EECS483') AND
+	  B.sid = E2.sid AND E2.cid = C2.cid AND (C2.title = 'EECS484' OR C2.title = 'EECS485') AND
+	  B.sid = E3.sid AND E3.cid = C3.cid AND C3.title = 'EECS280';
