868c412edba4d15a06072fb2192065b6f2a0685c
charlieluck <chaoshi@umich.edu>
2017-01-30 17:31:24 -0500

    fixes bugs in q1.sql

--- q1.sql
+++ q1.sql
@@ -1,23 +1,23 @@
 (
 SELECT S.sid FROM Student S, Enrolled E, Course C 
 WHERE S.sid = E.sid AND E.cid = C.cid 
-AND C.titie = 'EECS484'
+AND C.title = 'EECS484'
 INTERSECT 
 SELECT S.sid FROM Student S, Enrolled E, Course C 
 WHERE S.sid = E.sid AND E.cid = C.cid 
-AND C.titie = 'EECS485'
+AND C.title = 'EECS485'
 )
 UNION
 (
 SELECT S.sid FROM Student S, Enrolled E, Course C 
 WHERE S.sid = E.sid AND E.cid = C.cid 
-AND C.titie = 'EECS482'
+AND C.title = 'EECS482'
 INTERSECT 
 SELECT S.sid FROM Student S, Enrolled E, Course C 
 WHERE S.sid = E.sid AND E.cid = C.cid 
-AND C.titie = 'EECS486'
+AND C.title = 'EECS486'
 )
 UNION
 SELECT S.sid FROM Student S, Enrolled E, Course C 
 WHERE S.sid = E.sid AND E.cid = C.cid 
-AND C.titie = 'EECS281';
+AND C.title = 'EECS281';
