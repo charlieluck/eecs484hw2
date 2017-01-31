a241346cba2e1a418ae7777a26a2058fe9c0025a
刘唱白 <liuchangbai@0587381832.wireless.umich.net>
2017-01-30 21:04:27 -0500

    <

--- q4.sql
+++ q4.sql
@@ -4,5 +4,5 @@ FROM STUDENT S1, STUDENT S2, ENROLLED E1, ENROLLED E2
 WHERE NOT EXISTS (SELECT * FROM MEMBER M1, MEMBER M2
   			WHERE S1.sid = M1.sid AND S2.sid = M2.sid 
   			AND M1.pid = M2.pid) AND
-	  S1.sid = E1.sid AND S2.sid = E2.sid AND E1.cid = E2.cid AND S1.sid <> S2.sid
+	  S1.sid = E1.sid AND S2.sid = E2.sid AND E1.cid = E2.cid AND S1.sid < S2.sid 
 ORDER BY sid1, sid2;
