29a0fabded58150d46ad092d87da1dc504877476
刘唱白 <liuchangbai@0587381832.wireless.umich.net>
2017-01-30 20:41:23 -0500

    p4

--- q4.sql
+++ q4.sql
@@ -2,6 +2,6 @@ CREATE VIEW StudentPairs AS
 SELECT DISTINCT S1.sid, S2.sid
 FROM STUDENT S1, STUDENT S2, ENROLLED E1, ENROLLED E2
 WHERE S1.sid = E1.sid AND S2.sid = E2.sid AND E1.cid = E2.cid AND S1.sid <> S2.sid
-AND ( S1.sid, S2.sid NOT IN 
-( SELECT S3, S4 FROM STUDENT S3, STUDENT S4, MEMBER M3, MEMBER M4
-  WHERE S3.sid = M3.sid AND S4.sid = M4.sid AND M3.pid = M4.pid AND S3.sid <> S4.sid));
+AND NOT IN (SELECT * FROM MEMBER M1, MEMBER M2
+  			WHERE S1.sid = M1.sid AND S2.sid = M2.sid 
+  			AND M1.pid = M2.pid);
